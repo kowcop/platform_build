52b72d3208d7c192492dde5209c2ebad1b032ab9
kowcop <kowcop+@gmail.com>
2013-11-30 16:57:21 +1100

    Revert "ota release tools: override device assert"
    This reverts commit 8ebdad7fa706a9c52d9bd897b79ca19e0ff13a4e.

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -92,10 +92,10 @@ class EdifyGenerator(object):
 
   def AssertDevice(self, device):
     """Assert that the device identifier is the given string."""
-    cmd = ('assert(' +
-           ' || \0'.join(['getprop("ro.product.device") == "%s" || getprop("ro.build.product") == "%s"'
-                         % (i, i) for i in device.split(",")]) +
-           ');')
+    cmd = ('getprop("ro.product.device") == "%s" || '
+           'abort("This package is for \\"%s\\" devices; '
+           'this is a \\"" + getprop("ro.product.device") + "\\".");'
+           ) % (device, device)
     self.script.append(cmd)
 
   def AssertSomeBootloader(self, *bootloaders):
